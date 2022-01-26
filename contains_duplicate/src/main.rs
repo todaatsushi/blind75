86f1a909cb1b1ae5ca18504a75197b572887a924
Atsushi Toda <todaatsushi0@gmail.com>
2022-01-26 17:18:14 +0000

    Random main method: do i need this?

--- contains_duplicate/src/main.rs
+++ contains_duplicate/src/main.rs
@@ -1,1 +1,10 @@
-fn main() {}
+mod contains_duplicate;
+
+fn main() {
+    let mut nums: Vec<i32> = Vec::new();
+    nums.push(1);
+    nums.push(1);
+    nums.push(2);
+
+    println!("{}", contains_duplicate::contains_duplicates(nums));
+}
