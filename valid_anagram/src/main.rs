3d10a1269508d4a668a1ac61c36434ef5076d893
Atsushi Toda <todaatsushi0@gmail.com>
2022-01-27 22:54:23 +0000

    Arbitary main

--- valid_anagram/src/main.rs
+++ valid_anagram/src/main.rs
@@ -1,3 +1,9 @@
+mod check_valid;
+mod frequency_map;
+
 fn main() {
-    println!("Hello, world!");
+    let rat: String = "rat".to_string();
+    let car: String = "car".to_string();
+
+    println!("{:?}", check_valid::valid_anagram(rat, car));
 }
