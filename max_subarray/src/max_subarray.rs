4291f8d264bd612b62cd67d683e9f0d3f7e904ed
Atsushi Toda <todaatsushi0@gmail.com>
2022-02-02 22:21:03 +0000

    LC-53: solve *

--- max_subarray/src/max_subarray.rs
+++ max_subarray/src/max_subarray.rs
@@ -1,6 +1,21 @@
+use std::cmp;
+
 pub fn get_max_subarray(nums: Vec<i32>) -> i32 {
-    println!("{:?}", nums);
-    101
+    if nums.len() == 1 {
+        return nums[0];
+    }
+
+    let mut sub_total: i32 = nums[0];
+    let mut prefix: i32 = 0;
+
+    for num in nums.iter() {
+        prefix = cmp::max(0, prefix);
+        prefix += num;
+
+        sub_total = cmp::max(sub_total, prefix);
+    }
+
+    sub_total
 }
 
 #[cfg(test)]
@@ -25,7 +40,7 @@ mod tests {
 
     #[test]
     fn test_gets_correct_total_with_negative_nums() {
-        let nums: Vec<i32> = vec![-2, -1, -3, -4, -1, -2, -1, -5, 4];
+        let nums: Vec<i32> = vec![-2, -1, -3, -4, -1, -2, -1, -5, -4];
         let expected: i32 = -1;
 
         assert_eq!(expected, get_max_subarray(nums));
