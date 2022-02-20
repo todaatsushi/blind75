81db73aca708c7a98e89fbd0c76de6b200ab2e77
Atsushi Toda <todaatsushi0@gmail.com>
2022-02-20 19:45:18 +0000

    Add tests

---
+++ 3sum-ts/src/threeSum.test.ts
@@ -0,0 +1,22 @@
+import threeSum from "./threeSum"
+
+test("Test nums creates an array with the correct results", () => {
+  const nums: Array<number> = [-1,0,1,2,-1,-4]
+  const expected: Array<Array<number>> = [[-1, 2, -1], [-1, 0 ,1]]
+
+  expect(threeSum(nums)).toBe(expected)
+})
+
+test("Empty input returns no results", () => {
+  const nums: Array<Any> = []
+  const expected: Array<Any> = []
+
+  expect(threeSum(nums)).toBe(expected)
+})
+
+test("No answer input returns no results", () => {
+  const nums: Array<Any> = [0]
+  const expected: Array<Any> = []
+
+  expect(threeSum(nums)).toBe(expected)
+})
